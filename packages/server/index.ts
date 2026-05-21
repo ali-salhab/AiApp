5e3a1e0c972d82cda736c7b6b85d129b6c0459d7
ali-salhab <alysalhab5@gmail.com>
2026-05-21 19:17:48 +0300

    initialize project structure with Bun, add .gitignore, README, and configuration files

---
+++ packages/server/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import type {Request,Response} from "express";
+import dotenv from "dotenv";
+// this read all the environment variable from .env file and add to process.env
+dotenv.config();
+
+const app=express();
+const port =process.env.PORT || 3000;
+
+
+app.get("/",(req:Request,res:Response)=>{
+    res.send("");
+
+})
+
+
+app.listen(port,()=>{
+    console.log(`Server  is is running on port http://localhost:${port}`);
+})
