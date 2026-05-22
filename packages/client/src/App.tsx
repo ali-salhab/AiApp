900bb1241399e10f216be84b6318322524ea680b
ali-salhab <alysalhab5@gmail.com>
2026-05-22 10:46:01 +0300

    feat: add Prettier for code formatting and update server API endpoints

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,19 +1,24 @@
-
-import './App.css'
-import { useEffect, useState } from 'react'
+import './App.css';
+import { useEffect, useState } from 'react';
 import { Button } from './components/ui/button';
 function App() {
-const [message, setMessage] = useState('Hello, World!');
-useEffect(()=>{
-  fetch('/api/hello').then(res => res.json()).then(data => setMessage(data.message));
-},[])
+  const [message, setMessage] = useState('Hello, World!');
+  useEffect(() => {
+    fetch('/api/hello')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
 
-
-return<div className="p-4">
-<p className="font-bold bg-red-500 text-white">{message}</p>
-<Button>Clike me </Button>
-</div>
-
+  return (
+    <div className="p-4">
+      <p className="font-bold bg-red-500 text-white">{message}</p>
+      <Button>Clike me </Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+//
+
+//
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,25 +1,21 @@
-import express from "express";
-import type {Request,Response} from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
 // this read all the environment variable from .env file and add to process.env
 dotenv.config();
 
-const app=express();
-const port =process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('');
+});
+app.get('/api/hello', (req: Request, res: Response) => {
+  res.json({
+    message: 'Hello from the server!',
+  });
+});
 
-app.get("/",(req:Request,res:Response)=>{
-    res.send("");
-
-})
-app.get("/api/hello",(req:Request,res:Response)=>{
-    res.json({
-        message:"Hello from the server!"
-    });
-
-})
-
-
-app.listen(port,()=>{
-    console.log(`Server  is is running on port http://localhost:${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server  is is running on port http://localhost:${port}`);
+});
